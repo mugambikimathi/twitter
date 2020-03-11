490287d2cf501e8f70665155280fbd66855cb6c6
mugambi <mugambi.kimathi@gmail.com>
2020-03-11 10:19:32 +0300

    trends settings

--- src/components/trends/TrendsComponent.vue
+++ src/components/trends/TrendsComponent.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   div()
-      v-form
-            v-text-field.form1(placeholder='Search Twitter', filled='' rounded='', dense='')
-      v-card.background()
-        v-card-text.card
-                v-list-item-title.h1 Trends for you
-        
+    v-form
+      v-text-field.form1(prepend-icon="search" placeholder='Search Twitter', filled, rounded, dense)
+
+    v-card.background()
+      v-card-text.card
+        v-list-item-title.h1 Trends for you
+      
 
 
 </template>
@@ -15,11 +16,11 @@ export default {};
 </script>
 
 <style>
-.form1{
-  width: 325px;
+.form1 {
+  /* width: 325px; */
 }
-.card{
-  background-color:#E2EAF8;
+.card {
+  background-color: #e2eaf8;
   color: #000000;
   font-weight: bold;
   font-size: 15px;
